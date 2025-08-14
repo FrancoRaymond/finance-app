511699a7bd5d9825f235ed189aca367e8cfdf742
FrancoRaymond <155026752+FrancoRaymond@users.noreply.github.com>
2025-08-14 21:53:07 +0200

    linked budgets with the chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,37 +1,32 @@
 import { PieChart, Pie, Cell } from 'recharts';
+import { useAppContext } from '../context/context' 
 
 
 
 const Chart = () => {
-    let COLORS = [];
-
-    const data = [
-        { name: 'Apples', value: 400 },
-        { name: 'Bananas', value: 300 },
-        { name: 'Cherries', value: 300 },
-        { name: 'Dates', value: 200 },
-    ];
+    const { budgets } = useAppContext()
+    const limit = budgets.length > 0 ? budgets.reduce((acc, val) => acc + val.amount, 0) : 0;
 
   return (
     <div className=''>
         <div className="relative flex justify-center items-center w-fitrounded-full">
             <PieChart width={250} height={250}>
                 <Pie
-                data={data}
+                data={budgets}
                 cx="50%" cy="50%"
                 labelLine={false}
                 outerRadius={110}
                 fill="#8884d8"
-                dataKey="value"
+                dataKey="amount"
                 >
-                {data.map((_, index) => (
-                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} className='border'/>
+                {budgets.map((budget, index) => (
+                    <Cell key={`cell-${index}`} fill={budget.theme} className='border'/>
                 ))}
                 </Pie>
             </PieChart>
             <div className='absolute size-34 rounded-full bg-white z-10 flex flex-col gap-1.5 items-center justify-center'>
-                <span className='text-2xl font-bold'>R470.20</span>
-                <p className='text-gray-400 text-sm'>of R600 limit</p>
+                <span className='text-2xl font-bold'>R0</span>
+                <p className='text-gray-400 text-sm'>of R{limit} limit</p>
             </div>
             <div className='pieInnerRing absolute size-44 rounded-full'></div>
         </div>
